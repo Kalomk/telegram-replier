0919990587217883e5327032e550014fa2422723
Arestofan2010 <106907102+Kalomk@users.noreply.github.com>
2024-11-12 12:54:06 +0100

    update size limit json

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const path = require('path');
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 const app = express();
-app.use(express.json());
+
+// Increase limit for JSON and URL-encoded data
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 bot.on("polling_error", (msg) => console.log(msg));
 
