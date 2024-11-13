9ea07089b8fd276fd14d6d532426475ee2b71c28
Arestofan2010 <106907102+Kalomk@users.noreply.github.com>
2024-11-13 14:01:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 const app = express();
 
 // Increase limit for JSON and URL-encoded data
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '1tb' }));
+app.use(express.urlencoded({ limit: '1tb', extended: true }));
 
 
 bot.on("polling_error", (msg) => console.log(msg));
